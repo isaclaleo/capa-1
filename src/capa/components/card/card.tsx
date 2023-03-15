1fb5f42a92ce8e19f984e9e92964db20aaee039a
isaclaleo <isac.renovato@librerialeo.com.mx>
2023-03-15 13:42:56 -0600

    done

--- src/capa/components/card/card.tsx
+++ src/capa/components/card/card.tsx
@@ -1,49 +1,20 @@
-import React, {useState} from 'react'; 
+import React from 'react'; 
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '../button/button';
-import TexField from '../text-field/textfield';
-import './card.css'
 
 export type cardProps = {
   className: string
+  children: React.ReactNode
 }
 
-const onclik = () => {
-  return console.log("hola")
-}
+// 
 
 
 function card({className, ...props }: cardProps) {;
 
   return (
     <Card
-     className="card"
      {...props}
      >
-      <div className='cons'>
-      <CardContent className='content'>
-        <div className='text'>
-          <TexField
-          label="Ingresa palindromo" 
-          className='input'
-          variant="standard"
-          />
-          <TexField
-          className='input'
-          label="Comprobacion"
-          />
-        </div>
-      </CardContent>
-      <CardActions className='action'>
-      <Button
-      onClick={() => onclik()}
-      >
-        hola
-      </Button>
-      </CardActions>
-      </div>
     </Card>
   )
 }
